b7edb780e6436f73f69bace2de20d872d4c404ff
Jayde Mike <jadeengracia00@gmail.com>
2023-02-22 22:37:07 +0800

    Added Feature Intro Page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Dashboard from './Pages/Dashboard';
 import AddItem from './Pages/addItem';
@@ -6,6 +6,7 @@ import EditItem from './Pages/editItem';
 import DeleteItem from './Pages/deleteItem';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import sarisari from '../src/images/cart.gif'
 
 // import react router
 import {
@@ -17,7 +18,7 @@ import App from './App';
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App/>,
+        element: <IntroductionScreen/>,
     },
     {
         path: "Dashboard",
@@ -36,6 +37,33 @@ const router = createBrowserRouter([
         element: <DeleteItem />,
       }    
 ])
+
+function IntroductionScreen() {
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setRedirect(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (redirect) {
+    return <Dashboard />
+  }
+
+  return(
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh', background: '#35475F' }}>
+      <img src={sarisari} style={{ maxWidth: '100%', height: 'auto' }} />
+      <h1 style={{color: 'white', letterSpacing: '5px'}}>Sari-Sari Store</h1>
+    </div>
+
+
+  )
+
+
+}
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
