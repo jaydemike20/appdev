ab5a67bcb4ece6e3527829072c7e14ddc1f96430
Bohari Ambulo <bohariambulo17@gmail.comm>
2023-02-22 21:33:19 +0800

    added DeleteItem UI

--- src/Pages/deleteItem/index.js
+++ src/Pages/deleteItem/index.js
@@ -1,12 +1,60 @@
+import React, {useState} from 'react'
 import Navbar from "../../Component/Navbar/index";
-
-
+import SearchBar from "../../Component/Searchbar";
+import ProductData from '../../Data/data';
+import './index.css';
+import Modal from 'react-modal';
+import warningicon from "../../Component/icon/warning-icon.png"
 
 function DeleteItem() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const handleDelete = (value, index) => {
+        setModalIsOpen(true);
+    };
+    
     return(
         <div>
             <Navbar />
-            <div>Delete Item Ni - new branch deleleitem</div>        
+            
+            <div className = "centered">
+                <div className = "searchbarr">
+                    <SearchBar/>
+                </div>
+            </div>
+            <div className = "centeredd">
+                <div className = "productss">
+                    <div className='roww'>
+                    {ProductData.map((value, index) => (
+                        
+                            <div className='columnn' key={index}>
+                                
+                                <div className='cardd'>
+                                
+                                    <img src={value.img} style={{width: 100, height: 163}}  className="productimage" />            
+                    
+                                    <div className='containercardd'>
+                                        <h4>{value.name}</h4>
+                                        <p> ₱{value.price}.00</p>
+                                        <button className='delete' onClick={handleDelete}>Delete</button>
+                                        <Modal className = 'modal' isOpen={modalIsOpen} overlayClassName="modal-overlay" >
+                                            <img src={warningicon} className="warning"/>  
+                                            <h2>Are you sure to delete<br></br> {value.name}? </h2>
+                                            <div className = 'buttons'>
+                                                <button className = 'cancel' onClick={() => setModalIsOpen(false)}>CANCEL</button>
+                                                <button className = 'yes' onClick={() => setModalIsOpen(false)}>YES</button>
+                                            </div>
+                                            
+                                        </Modal>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </div>
+            </div>  
+            
+                    
         </div>
     );
 }
