84ef21eb75d7dd64d2ed5cea66bb6110ba3ae2f2
Bohari Ambulo <bohariambulo17@gmail.com>
2023-02-27 19:16:20 -0800

    added editItem UI (unfinished)

--- src/Pages/editItem/index.js
+++ src/Pages/editItem/index.js
@@ -1,11 +1,45 @@
 import Navbar from "../../Component/Navbar/index";
-
-
+import React, {useState} from 'react'
+import SearchBar from "../../Component/Searchbar";
+import './index.css';
+import ProductData from '../../Data/data';
 function EditItem() {
     return(
         <div>
             <Navbar />
-            <div>Edit Item Ni - new branch editItem</div>        
+            <div className = "centered">
+                <div className = "searchbarr">
+                    <SearchBar/>
+                </div>
+            </div>
+            <div className = "centeredd">
+                <div className = "productss">
+                    <div className='roww'>
+                    {ProductData.map((value, index) => (
+                        
+                            <div className='columnn' key={index}>
+                                
+                                <div className='cardd'>
+                                
+                                    <img src={value.img} style={{width: 100, height: 163}}  className="productimage" />            
+                    
+                                    <div className='containercardd'>
+                                        <h4>{value.name}</h4>
+                                        <p> ₱{value.price}.00</p>
+                                        <button className='edit'>Edit</button>
+                                        
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        ))}
+                         <div className="edititem">
+                            
+                         </div>
+                </div>
+               
+            </div>
+            </div> 
         </div>
     );
 }
